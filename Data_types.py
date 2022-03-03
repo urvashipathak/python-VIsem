c0c826347a52b5d5f8cd536b1bed618f76d0eadb
urvashipathak <91876663+urvashipathak@users.noreply.github.com>
2022-03-03 16:33:46 +0530

    Create Data_types.py

---
+++ Data_types.py
@@ -0,0 +1,35 @@
+num=100
+
+print(num1)
+/100
+
+String = 'Acropolis'
+
+print(String1) 
+//Acropolis
+
+fnum=2.5
+
+print(fnum)
+//2.5
+
+a = 2
+b = 2
+c= a+b
+print(c)
+//4
+
+list =[1,2,3,4,5]
+
+print(list)
+//12345
+
+tuple=(1,2,3,4)
+
+print(t)
+//1234
+
+dic={1:"ID",2:"Name",3:"Dept"}
+
+print(dic[1])
+// ID 
